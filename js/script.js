ca4b063e13cda620c3eb7780f2a33bf0c315bff5
Lujanhi <51241524+Lujanhi@users.noreply.github.com>
2019-06-12 10:56:53 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ var quotes = [{
 {//Quote # 3 //
   quote: "Expect nothing, live frugally on surprise",
   source: "Alice Walker",
-  citation: " ",
+  // no CITATION to test IF statement
   year: "1963,",
   tags: "tag 3",// EXTRA CREDIT//
 },
@@ -37,55 +37,51 @@ var quotes = [{
   source: "Anais Nin",
   citation: "website",
   year: "1965,",
-  tags: " ",// EXTRA CREDIT//
+  // no tags to test the IF statement
 }];
 
 
 
 //// generating a random quote generator///
 
-function getRandomQuote(quotes) {                                        // this function call in the global variable or a specify global variable ,but sometimes this is not need if you have more than 1 variable, you can CALL ALL the global varibles but you need a specify name to call all global variables.
-  var randomQuotesNumber = Math.floor(Math.random() * quotes.length);      // Math.floor = to rounding the # down // Math.random() just gives you a random number this includes decimal(2.3455) // quotes.length is the amount inside the things you inside the variable, example color = quote 1 quote 2 quote 3
+function getRandomQuote(quotes) {                                               // this function call in the global variable or a specify global variable ,but sometimes this is not need if you have more than 1 variable, you can CALL ALL the global varibles but you need a specify name to call all global variables.
+  var randomQuotesNumber = Math.floor(Math.random() * quotes.length);           // Math.floor = to rounding the # down // Math.random() just gives you a random number this includes decimal(2.3455) // quotes.length is the amount inside the things you inside the variable, example color = quote 1 quote 2 quote 3
 
-  return quotes[randomQuotesNumber];
-}                                     // return is when the computer talks back to you  
+  return quotes[randomQuotesNumber];}                                           // return is when the computer talks back to you  
 
-var results = getRandomQuote(quotes);                                   // gives you a result of the function ,BUT you must call it INSIDE the () to get a answer
-console.log(results);                                                   // prints the answer to the console on the website
+var results = getRandomQuote(quotes);                                           // gives you a result of the function ,BUT you must call it INSIDE the () to get a answer
+console.log(results);                                                           // prints the answer to the console on the website
 
 
 //////// print quote function //////////
 
-function printQuote() {      // the function printQuote will do the function of printing the quotes, there is nothing on the () becasue we only have 1 global entry and you can call it in in the function.
-  var randomQuote = getRandomQuote(quotes); //this variables cannot be used out side this {objects} this call the global function
-  var message = " ";                  // this is not need and the page will website will work.
-  message += "<p class='quote'>" + randomQuote.quote + '</p>'; //prints out the qoute name
-  message += "<p class='source'>" + randomQuote.source;      // prints out the source name
+function printQuote() {                                                         // the function printQuote will do the function of printing the quotes, there is nothing on the () becasue we only have 1 global entry and you can call it in in the function.
+  var randomQuote = getRandomQuote(quotes);                                     //this variables cannot be used out side this {objects} this call the global function
+  var message = " ";                                                            // this is not need and the page will website will work.
+  message += "<p class='quote'>" + randomQuote.quote + '</p>';                  //prints out the qoute name
+  message += "<p class='source'>" + randomQuote.source;                         // prints out the source name
 
   if (randomQuote.citation) {
     message += "<span class='citation'>" + randomQuote.citation + "</span>";    // the IF statement works (if) one of the properties does not work, this will allow to keep running the quote
   }
   if (randomQuote.year) {
-    message += "<span class='year'>" + randomQuote.year + "</span>";               // same as the other one up top
+    message += "<span class='year'>" + randomQuote.year + "</span>";            // same as the other one up top
   }
 
   if (randomQuote.tags) {
     message += "<span class='tags'>" + randomQuote.tags + "</span>";            // same as the other one up top
   };
 
-  message += "</p>";               // <p>    </p> this works as a  paragraph makes a new line to info in it.
+  message += "</p>";                                                            // <p>  </p> this works as a  paragraph makes a new line to info in it.
 
-  document.getElementById('quote-box').innerHTML = message;       // this allows javascript to go inside HTML to input the information or source 
+  document.getElementById('quote-box').innerHTML = message;                     // this allows javascript to go inside HTML to input the information or source 
 
+  //extra credit
+  //Random background color
+  //When the quote changes, randomly change the background color of the page.
 
-
-  ////////////////////////////////////////////////////????????????????????????????????????????
-  //?????????????????????????????????????????????????????????/////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////////////
-
-  // random color function, to get a number from 1-255 
   function colorChange() {
-    return Math.floor(Math.random() * 256)
+    return Math.floor(Math.random() * 256) // random color function, to get a number from 1-255 
   }
   var x = colorChange();
   var y = colorChange();
@@ -94,9 +90,17 @@ function printQuote() {      // the function printQuote will do the function of 
 
   document.body.style.background = bgColor;
 };
-// finds the "id" Element
+              // finds the "id" Element
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-//Calls the printQuote function.
+              //Calls the printQuote function.
 printQuote()
+//extra credit
+//Auto-refresh the quote
+//After a set amount of time, like 20 to 30 seconds, print a new quote to the page. A timing method like setInterval() is helpful here. Avoid using the setTimeout function for this step. See the links in the “Additional Resources” section.
 
+// refresh frunction
+function refresh() {
+  var timer = setInterval(printQuote, 20000);            // sets the timer to (every 20 seconds)
+}
+  refresh();                                             // calls the refresh
